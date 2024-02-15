dda4508853a818012ae661ae897869b546189f87
mrgoon69 <160111637+mrgoon69@users.noreply.github.com>
2024-02-15 00:34:26 -0800

    Add files via upload

---
+++ P1LAB1_RodriguezNicholas.py
@@ -0,0 +1,14 @@
+# Nicholas Rodriguez
+
+# 14 Feb. 2024
+
+# P1LAB1
+
+# Program that takes input and outputs a welcome message.
+
+print('Enter name:', end=' ')
+your_name = input()
+
+
+print('Hey', your_name)
+print('Welcome to zyBooks!')
